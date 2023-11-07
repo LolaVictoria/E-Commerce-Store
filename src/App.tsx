98ac4a959e196b27fc985e1d28e88e790bec9368
LolaVictoria <dammyvicky1245@gmail.com>
2023-11-07 16:53:08 +0100

    debug errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-git addimport { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import HealthAndBeauty from "./pages/health&beauty";
 import HomeAndOffice from "./pages/home&offices";
