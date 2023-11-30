893a1734a3b8dffebaa4fd2d727e3e38f229cb37
Damilola Oniyide <dammyvicky1245@gmail.com>
2023-11-30 22:03:58 +0000

    changed ui interface

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,41 @@
 const Footer = () => {
     return (
-        <div className="bg-[#181818] w-[100%] h-24 mt-4">
+        <footer className="bg-[#181818] w-[100%] px-20 py-6 text-[#fff] mt-4 grid grid-cols-3">
+               <div>
+                  <h3 className="font-semibold mb-4">Help</h3>
+                  <ul className="text-gray-200 text-sm">
+                    <li className="">Returns Policy</li>
+                    <li>Start a Return</li>
+                    <li>Refund</li>
+                    <li>Shopping Info</li>
+                    <li>How to Order</li>
+                    <li>Track Order</li>
+                    <li>Contact Us</li>
+                    <li>FAQs</li>
 
-        </div>
+                  </ul>
+               </div>
+
+               <div>
+                  <h3 className="font-semibold mb-4">Privacy & Legal</h3>
+                  <ul className="text-gray-200 text-sm">
+                    <li>Privacy Policy</li>
+                    <li>Terms & Conditions</li>
+                  </ul>
+               </div>
+
+               <div>
+                  <h3 className="font-semibold mb-4">Stay Updated</h3>
+                  <p className="text-sm font-normal">Subscribe to our newsletter to get latest update about us</p>
+                 <div className="mt-8">
+                  <input 
+                    type="text"
+                    className="py-3 px-2 rounded-l-lg text-[#181818] focus:outline-none"  />
+                   <button
+                      className="bg-[#2ECF5A] py-3 px-2 rounded-r-lg">Subscribe</button>
+                 </div>
+               </div>
+        </footer>
 
     );
 }
--- src/components/products.tsx
+++ src/components/products.tsx
@@ -13,22 +13,20 @@ type productObj = {
 const Products: React.FC<productObj> =({img, title, category, price, ratings, id}) => {
     return (
         
-                <div className="relative bg-[#2ECF5A] w-60 h-auto rounded-lg">
-                    <img src={img} alt={title} className="w-60 h-[75%] rounded-t-lg" />
+                <div className="relative text-[#181818] w-60 h-auto rounded-lg">
+                    <img src={img} alt={title} className="w-60 h-72 object-fit" />
                     <img src={FavoriteIcon} alt="Favorite" className="absolute top-5 right-5" />
-                    <div className="flex flex-col justify-center items-center my-3">
-                    <p className="text-[#FFF] text-lg font-normal">{title}</p>
-                         <p className="text-[#FFF] text-lg font-normal">{category}</p>
-                        <p className="text-[#FFF] text-lg font-normal">{formatCurrency(price)}</p>
-                        <p className="text-[#FFF] text-lg font-normal">{ratings}</p>
+                    <div className="flex flex-col justify-center items-center my-1">
+                    <p className="text-lg font-bold">{title}</p>
+                        <p className="text-md font-normal">{formatCurrency(price)}</p>
+                        <div className="flex">
+                         <p className="text-md font-normal mr-3">{category}</p>
+                         <p className="text-md font-normal ml-3">{ratings}</p>
+                        </div>
 
-                        <Link to={`/productdetails/${id}`}>
-                        <a
-                            href={`/product/${id}`}
-                            className="bg-[#181818] rounded-lg w-24 h-8 my-2 text-[#FFF] font-semibold text-base"
-                        >
-                            See Details
-                        </a>
+                        <Link to={`/productdetails/${id}`}                       
+                            className="bg-[#181818] rounded-lg px-3 py-2 mt-2 mb-2 text-[#FFF] font-semibold text-base">                        
+                            See Details                       
                         </Link>
                     </div>
                 </div>
